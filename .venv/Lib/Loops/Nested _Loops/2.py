92140ffbc3dc595fc5562971cd42f60eefd6d88c
JuliaSCK <118895869+JuliaSCK@users.noreply.github.com>
2025-04-09 16:13:26 +0300

    study

--- .venv/Lib/Loops/Nested _Loops/2.py
+++ .venv/Lib/Loops/Nested _Loops/2.py
@@ -1,4 +1,4 @@
-height = int(input("Enter the number: "))  # 1. קולטים מהמשתמש את גובה הפירמידה
+height = int(input("Enter the height of the pyramid: "))  # 1. קולטים מהמשתמש את גובה הפירמידה
 
 for i in range(1 ,height + 1):  # 2. לולאה חיצונית: רצה על כל שורה בפירמידה
     spaces = height - i   # 3. חישוב מספר הרווחים - מחשבת את מספר הרווחים שצריך להדפיס לפני הכוכביות בשורה הנוכחית
@@ -8,8 +8,18 @@ for i in range(1 ,height + 1):  # 2. לולאה חיצונית: רצה על כל
     print("*" * stars)  # מדפיסים כוכביות ויורדים שורה
 
 
-height_2 = int(input("Enter the number: "))
+height_2 = int(input("Enter the height of the pyramid: "))
 
 for i in range(1, height_2 + 1):
     print(" " * (height_2 - i) + "*" * (2 * i - 1))
+
+
+height_3 = int(input("Enter the height of the pyramid: "))
+
+for i in range(1, height_3 + 1):
+    for j in range(height_3 - i):
+        print(" ", end="")
+    for j in range(2 * i - 1):
+        print("*", end="")
+    print()
 
