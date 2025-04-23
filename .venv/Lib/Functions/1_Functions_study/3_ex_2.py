a02565eae7af50e3bb1552c9fc44d4671514c68e
JuliaSCK <118895869+JuliaSCK@users.noreply.github.com>
2025-04-23 22:37:10 +0300

    functions

--- .venv/Lib/Functions/1_Functions_study/3_ex_2.py
+++ .venv/Lib/Functions/1_Functions_study/3_ex_2.py
@@ -1,10 +1,10 @@
-def count_vowels (s):
-    vowels = "aeiouyAEIOUY"
-    count = 0
+def count_vowels (text_input):
+    vowels = "aeiouyAEIOUY"  # מגדירים את התנועות לבדיקה
+    count = 0  # מאתחלים את הספירה
 
-    for char in s:
-        if char in vowels:
-            count += 1
+    for char in text_input:    # עוברים על כל תו במחרוזת הקלט
+        if char in vowels:  # # בודקים אם התו הוא תנועה
+            count += 1  # # אם כן, מגדילים את הספירה
 
-    return count
+    return count  # מחזירים את הספירה הסופית
 print(count_vowels("Hey, I am Yuliya"))
